e4c81f246ca221bd3ea7e2eda951df8b0e3cf9c5
Austin Johanningsmeier <ajohanni@umich.edu>
2020-01-05 22:14:22 -0500

    added sub components for skills and projects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,23 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { SkillsComponent } from './skills/skills.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { EducationComponent } from './education/education.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { SkillItemComponent } from './skill-item/skill-item.component';
+import { ProjectItemComponent } from './project-item/project-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     WelcomeComponent,
-    SkillsComponent
+    SkillsComponent,
+    ExperienceComponent,
+    EducationComponent,
+    ProjectsComponent,
+    SkillItemComponent,
+    ProjectItemComponent
   ],
   imports: [
     BrowserModule,
